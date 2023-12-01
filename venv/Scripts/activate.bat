0b5d101852adef2507c005dfbe50c0679dd1d7fd
tamim201120022 <tamim201120022@student.ndub.edu.bd>
2023-12-01 19:04:02 +0600

    first commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Anik\Desktop\Tamim\project_folder\venv
+set VIRTUAL_ENV=F:\Thesis\Tamim\project_folder\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
